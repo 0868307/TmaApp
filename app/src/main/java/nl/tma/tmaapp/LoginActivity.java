6edac6d8c74280f93eb5983fa0f478ae0d89abd1
jelle <jellevdtoren@hotmail.com>
2016-05-26 11:01:27 +0200

    update voor ik naar school ga

--- app/src/main/java/nl/tma/tmaapp/LoginActivity.java
+++ app/src/main/java/nl/tma/tmaapp/LoginActivity.java
@@ -315,23 +315,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         @Override
         protected Boolean doInBackground(Void... params) {
             // TODO: attempt authentication against a network service.
-            boolean authenthicated =false;
-            try {
-                authenthicated = authenthicate();
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
             return true;
         }
         private boolean authenthicate(){
--- app/src/main/java/nl/tma/tmaapp/ProjectOverviewActivity.java
+++ app/src/main/java/nl/tma/tmaapp/ProjectOverviewActivity.java
@@ -1,40 +1,65 @@
 package nl.tma.tmaapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.SimpleCursorAdapter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ProjectOverviewActivity extends AppCompatActivity {
+    ListView myList;
 
-    @Override
+    private String responseData;
+
+    static class GitUser {
+        String username;
+        int points;
+    }
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_project_overview);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    setContentView(R.layout.activity_project_overview);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Gson gson = new Gson();
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("http://145.24.222.137:8092/ranking")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                 responseData = response.body().string();
+
+
             }
         });
+        String[] items = {responseData};
+        Log.d("Debug", responseData);
+        myList = (ListView) findViewById(R.id.projectListView);
+        myList.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items));
 
-        ListView listView = (ListView) findViewById(R.id.projectListView);
-        String[] strings = {"deafea","mf","1","2","3","4"};
-        listView.setAdapter(new ArrayAdapter<String>(getBaseContext(),R.layout.list_item,R.id.list_item_name,strings));
+
+
+
+
     }
-}
+
+
+
+    }
+
